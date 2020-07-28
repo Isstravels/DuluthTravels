a58e5045ed38c7e041542f44ff3c525562902620
Vamsi Garghi <vamsi@Vamsis-MacBook-Pro.local>
2020-07-28 17:47:10 -0500

    Updates

---
+++ precache-manifest.a9c6764725305463a8f0cb5db5d847e4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c6f75f7fc01a463a5845f3066f3b6e",
+    "url": "/DuluthTravels/index.html"
+  },
+  {
+    "revision": "c55cae5c0e52e160d629",
+    "url": "/DuluthTravels/static/css/main.1d75ce77.chunk.css"
+  },
+  {
+    "revision": "24db8a5fd9345536d8a2",
+    "url": "/DuluthTravels/static/js/2.9c62e41c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DuluthTravels/static/js/2.9c62e41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55cae5c0e52e160d629",
+    "url": "/DuluthTravels/static/js/main.0e9f9179.chunk.js"
+  },
+  {
+    "revision": "398d0ebeb05c4fc3426d",
+    "url": "/DuluthTravels/static/js/runtime-main.89f4918a.js"
+  },
+  {
+    "revision": "155502c4365f33e8f176e0b33553a188",
+    "url": "/DuluthTravels/static/media/Aqua.155502c4.jpg"
+  },
+  {
+    "revision": "30277f322b0cb262963682bb6229450e",
+    "url": "/DuluthTravels/static/media/Beach.30277f32.png"
+  },
+  {
+    "revision": "6dcc5d12d3bff3b8f90e1f5df82d01a0",
+    "url": "/DuluthTravels/static/media/Brew.6dcc5d12.jpg"
+  },
+  {
+    "revision": "4f420a339de13aa5a8e7bbf7c6c07d75",
+    "url": "/DuluthTravels/static/media/Bridge.4f420a33.jpg"
+  },
+  {
+    "revision": "e1564faed0b3ae36c0857a03e5e1c443",
+    "url": "/DuluthTravels/static/media/Chester.e1564fae.jpg"
+  },
+  {
+    "revision": "9aea3291201ebf2eaba540f069ed2f44",
+    "url": "/DuluthTravels/static/media/Duluth4.9aea3291.jpg"
+  },
+  {
+    "revision": "8205f109d1ba1b4af84948673fe071c1",
+    "url": "/DuluthTravels/static/media/Falls.8205f109.jpg"
+  },
+  {
+    "revision": "3af5e68a051b8f16f5c5d022d72f8a23",
+    "url": "/DuluthTravels/static/media/Hawk.3af5e68a.jpg"
+  },
+  {
+    "revision": "d4bc30f7cb62c8c0750fa51efeb375f1",
+    "url": "/DuluthTravels/static/media/Ice.d4bc30f7.jpg"
+  },
+  {
+    "revision": "f87cbad32fd95284a30e1067a1c965b3",
+    "url": "/DuluthTravels/static/media/Japan.f87cbad3.jpg"
+  },
+  {
+    "revision": "0f908690f91709da4f031ff581418054",
+    "url": "/DuluthTravels/static/media/Jay.0f908690.jpg"
+  },
+  {
+    "revision": "613ae29fea48198c1d8a7306fb19019f",
+    "url": "/DuluthTravels/static/media/London.613ae29f.jpg"
+  },
+  {
+    "revision": "3adb6e0f51ba06a59890f77904a580f9",
+    "url": "/DuluthTravels/static/media/Luce.3adb6e0f.jpg"
+  },
+  {
+    "revision": "737d26a90fb06c603212f194eef6ef12",
+    "url": "/DuluthTravels/static/media/P_malt.737d26a9.jpg"
+  },
+  {
+    "revision": "b4e8b96b4ebb15899e4e45277898caa2",
+    "url": "/DuluthTravels/static/media/Park.b4e8b96b.jpg"
+  },
+  {
+    "revision": "4c74fca76b23c6f77a619d6b589bd361",
+    "url": "/DuluthTravels/static/media/Rail.4c74fca7.jpg"
+  },
+  {
+    "revision": "6f6b44d622a3ce8fd8e82bcc9bb39fbf",
+    "url": "/DuluthTravels/static/media/Sheen.6f6b44d6.jpg"
+  },
+  {
+    "revision": "35d832e1dffa72affc8e73a895f7a098",
+    "url": "/DuluthTravels/static/media/Smoke.35d832e1.jpg"
+  },
+  {
+    "revision": "f65e357428c4143b404514ec3d6d7534",
+    "url": "/DuluthTravels/static/media/Sun.f65e3574.jpg"
+  },
+  {
+    "revision": "fab10c1cdc4110162d5f54396fd6610d",
+    "url": "/DuluthTravels/static/media/Sup.fab10c1c.png"
+  },
+  {
+    "revision": "d71ef9fdfd9dc29115686724c6925b5d",
+    "url": "/DuluthTravels/static/media/Tischer.d71ef9fd.jpg"
+  },
+  {
+    "revision": "f996dd148872620a791f37c7f8a48d09",
+    "url": "/DuluthTravels/static/media/Uncle.f996dd14.png"
+  },
+  {
+    "revision": "294f836fa1eee7f828aee9b19322e1f9",
+    "url": "/DuluthTravels/static/media/grill.294f836f.jpg"
+  },
+  {
+    "revision": "f061aabcf1dcd6764c66cf97cbbe1b71",
+    "url": "/DuluthTravels/static/media/zac.f061aabc.jpg"
+  }
+]);
